60e866fad13260c67bcd7221df1141e1ec202806
Daniil <lm56dr50@gmail.com>
2024-08-18 16:00:36 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function getIndex(char, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++){
+      if (bracketsConfig[i][0] == char || bracketsConfig[i][1] == char) return i;    
+  }
+}
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  if (str.length%2 != 0 || (str[0] == bracketsConfig[getIndex(str[0], bracketsConfig)][1] && bracketsConfig[getIndex(str[0], bracketsConfig)][1] != bracketsConfig[getIndex(str[0], bracketsConfig)][0])) return false;
+  
+  for (let i = 0; i < str.length; i++){
+    let a = getIndex(str[i], bracketsConfig);
+    if (stack.at(-1) == bracketsConfig[a][0] && bracketsConfig[a][1] == bracketsConfig[a][0]) stack.pop();
+    else if (str[i] == bracketsConfig[a][0]) stack.push(str[i]);
+    else if (stack.at(-1) != bracketsConfig[a][0]) return false;
+    else {
+      stack.pop();
+    }
+    
+  }
+  if (stack.length == 0) return true;
+  return false;
 }
